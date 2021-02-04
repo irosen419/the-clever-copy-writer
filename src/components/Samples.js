29997774c9de883c3a4bcbdae026dcd4762f77e8
irosen419 <irosen419@gmail.com>
2021-02-04 18:46:58 -0500

    reformat portfolio page

--- src/components/Samples.js
+++ src/components/Samples.js
@@ -4,11 +4,15 @@ function Samples() {
     return (
         <div className="samples">
             <h1>Portfolio</h1>
-            <h2 id="byline">Here is where you will find select samples of my work.</h2>
+            <p id="byline">Samples of my work</p>
             <div className="samplesInner">
-                {files.map(({ title, address }) =>
-                    <div className="sample" key={title}>
-                        <p>{title}</p> <a href={address} target="_blank" rel="noreferrer">View</a>
+                {files.map(({ title, address, preview }, i) =>
+                    <div className="sample" key={`sample-${i}`}>
+                        <div className="innerSample">
+                            <p className="title">{title}</p>
+                            <p className="preview">{preview}</p>
+                            <a href={address} target="_blank" rel="noreferrer">Continue reading...</a>
+                        </div>
                     </div>
                 )}
             </div>
--- src/data/sampleFiles.js
+++ src/data/sampleFiles.js
@@ -1,23 +1,28 @@
 const files = [
     {
-        'title': 'Restaurant Sample',
-        'address': '/samples/Restaurant Email.pdf'
+        'title': 'Restaurant',
+        'address': '/samples/Restaurant Email.pdf',
+        'preview': 'Hi Tom, \n\n This is Jane from Chill-o-Matic and I wanted to tell you about an unfortunate accident in the kitchen of one my favorite restaurants...'
     },
     {
         'title': 'Fish Oil Supplement',
-        'address': '/samples/Fish Oil Supplement.pdf'
+        'address': '/samples/Fish Oil Supplement.pdf',
+        'preview': 'Hey Susan, \n\n Tom here from Liquid Gold and I’ve got a little challenge for you... Do you think you can spot a phony when you see...'
     },
     {
         'title': 'Joyride Dog Harness',
-        'address': '/samples/Joyride Dog Harness.pdf'
+        'address': '/samples/Joyride Dog Harness.pdf',
+        'preview': 'Hi Lisa, \n\n Tony Philips here from JoyRide Harness and I have a question for you... do you remember the movie Halloween? What a horror classic...'
     },
     {
         'title': "Men's Dating Coach",
-        'address': "/samples/Men's Dating Coach.pdf"
+        'address': "/samples/Men's Dating Coach.pdf",
+        'preview': 'Hi Jim, \n\n Paul Wilson here from The Men’s Dating Coach...When’s the last time you did this? Many of us have been taught the safe and proper way...'
     },
     {
         'title': 'AWAI Build Your Own',
-        'address': '/samples/AWAI Build Your Own.pdf'
+        'address': '/samples/AWAI Build Your Own.pdf',
+        'preview': 'Dear Writer, \n\n I think you would agree that at some point in time, all businesses big or small need help to grow or even just to function effectively...'
     }
 
 ]
