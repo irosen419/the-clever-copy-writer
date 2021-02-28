ff47310e43dd78bc451dddf38fd1998a201b554b
irosen419 <irosen419@gmail.com>
2021-02-28 13:02:34 -0500

    change sample file names

--- src/data/sampleFiles.js
+++ src/data/sampleFiles.js
@@ -1,26 +1,26 @@
 const files = [
     {
-        'title': 'Restaurant',
+        'title': 'Restaurant Email',
         'address': '/samples/Restaurant Email.pdf',
         'preview': 'Hi Tom, \n\n This is Jane from Chill-o-Matic and I wanted to tell you about an unfortunate accident in the kitchen of one my favorite restaurants...'
     },
     {
-        'title': 'Fish Oil Supplement',
+        'title': 'Fish Oil Supplement Email',
         'address': '/samples/Fish Oil Supplement.pdf',
         'preview': 'Hey Susan, \n\n Tom here from Liquid Gold and I’ve got a little challenge for you... Do you think you can spot a phony when you see...'
     },
     {
-        'title': 'Joyride Dog Harness',
+        'title': 'Joyride Dog Harness Email',
         'address': '/samples/Joyride Dog Harness.pdf',
         'preview': 'Hi Lisa, \n\n Tony Philips here from JoyRide Harness and I have a question for you... do you remember the movie Halloween? What a horror classic...'
     },
     {
-        'title': "Men's Dating Coach",
+        'title': "Men's Dating Coach Email",
         'address': "/samples/Men's Dating Coach.pdf",
         'preview': 'Hi Jim, \n\n Paul Wilson here from The Men’s Dating Coach...When’s the last time you did this? Many of us have been taught the safe and proper way...'
     },
     {
-        'title': 'AWAI Build Your Own',
+        'title': 'AWAI Build Your Own with - Long Form Sales Letter',
         'address': '/samples/AWAI Build Your Own.pdf',
         'preview': 'Dear Writer, \n\n I think you would agree that at some point in time, all businesses big or small need help to grow or even just to function effectively...'
     }
